6af7c1da6f9f15e5d5ef91ead9b93e04495b0400
karvaneg <m.lecarvennec@cbnsa.fr>
2025-09-24 18:21:32 +0200

    ✨ F01: LOGINFORM > Add LoginForm.jsx + Add contenu

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function LoginForm() {
+
+    //state
+    const [inputValue, setInputValue] = useState('');
+
+    //comportements
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Bonjour ${inputValue} !`);
+        setInputValue('');
+    }
+
+    const handleChange = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    //affichage (render)
+    return (
+        <form action="submit" onSubmit={handleSubmit}>
+            <input 
+                value={inputValue} 
+                onChange={handleChange} 
+                type="text" 
+                placeholder="Entrez votre prénom..." 
+                required/>
+        </form>
+    );
+}
+    
