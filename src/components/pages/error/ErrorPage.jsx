a98152b4f76006445220185ffd31a83b3f4774b7
karvaneg <m.lecarvennec@cbnsa.fr>
2025-09-25 23:49:50 +0200

    ✨ F02: ERROR > add redirect to LoginPage

--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <>
+      <h1>Bonjour</h1>
+      <Link to="/"><button>Retourner vers la page d'accueil</button></Link>
+    </>
   )
 }
