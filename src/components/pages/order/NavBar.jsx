3b97e6258b059e3f932631ce8797cbc4864bab13
karvaneg <m.lecarvennec@cbnsa.fr>
2025-10-02 16:21:16 +0200

    💄 F04: ORDER > NavBar > Modify background color and add border-radius

--- src/components/pages/order/NavBar.jsx
+++ src/components/pages/order/NavBar.jsx
@@ -12,9 +12,10 @@ export default function NavBar({ username }) {
 }
 
 const NavbarStyled = styled.nav`
-    background: yellow;
+    background: white;
     height: 10vh;
     display: flex;
     justify-content: space-between;
     padding: 0 20px;
+    border-radius: 15px 15px 0px 0px;
 `;
