0450cbb51b0ca5007365362478325844d05e43c5
karvaneg <m.lecarvennec@cbnsa.fr>
2025-10-10 15:25:35 +0200

    💄 F07: ADMIN > Add AdminTabs and AdminPanel

--- src/components/pages/order/Main/Admin/Admin.jsx
+++ src/components/pages/order/Main/Admin/Admin.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
-import { theme } from "../../../../../theme";
+import AdminTabs from "./AdminTabs";
+import AdminPanel from "./AdminPanel";
 
 export default function Admin() {
   return (
     <AdminStyled>
-        <div className="admin-tabs">AdminTabs</div>
-        <div className="admin-panel">AdminPanel</div>
+        <AdminTabs />
+        <AdminPanel />
     </AdminStyled>
   )
 }
@@ -15,19 +16,4 @@ const AdminStyled = styled.div`
     bottom: 0;
     right: 0;
     left: 0;
-    height: 250px;
-    box-shadow: ${theme.shadows.subtle};
-    border: 1px solid ${theme.colors.greyLight};
-    background: ${theme.colors.white};
-
-
-    .admin-tabs {
-      //  border: 2px solid red;
-        
-    }
-
-    .admin-panel {
-      //  border: 2px solid orange;
-        
-    }
 `;
