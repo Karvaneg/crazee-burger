254c4f5eb7d4689f6a142933318a9a644df4c6ee
karvaneg <m.lecarvennec@cbnsa.fr>
2025-10-24 12:10:37 +0200

    🐛 F08: AdminPanel > Fix TextInput height by adding box-sizing

--- src/components/pages/order/Main/Admin/AdminPanel/AdminPanel.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AdminPanel.jsx
@@ -5,23 +5,21 @@ import OrderContext from "../../../../../../context/OrderContext";
 import { getTabsConfig, getTabSelected } from "../getTabsConfig";
 
 export default function AdminPanel() {
+  const { currentTabSelected } = useContext(OrderContext);
 
-  const { currentTabSelected } = useContext(OrderContext)
-
-  const tabs = getTabsConfig(currentTabSelected)
-  const tabSelected = getTabSelected(tabs, currentTabSelected)
+  const tabs = getTabsConfig(currentTabSelected);
+  const tabSelected = getTabSelected(tabs, currentTabSelected);
 
   return (
-    <AdminPanelStyled>
-        {tabSelected && tabSelected.Content}
-    </AdminPanelStyled>
-  )
+    <AdminPanelStyled>{tabSelected && tabSelected.Content}</AdminPanelStyled>
+  );
 }
 
 const AdminPanelStyled = styled.div`
-    height: 250px;
-    box-shadow: ${theme.shadows.subtle};
-    border: 1px solid ${theme.colors.greyLight};
-    background: ${theme.colors.white};
-    padding: 30px 5%;
+  height: 240px;
+  background: ${theme.colors.white};
+  border: 1px solid ${theme.colors.greyLight};
+  box-shadow: ${theme.shadows.subtle};
+  box-sizing: border-box;
+  padding: 30px 5%;
 `;
