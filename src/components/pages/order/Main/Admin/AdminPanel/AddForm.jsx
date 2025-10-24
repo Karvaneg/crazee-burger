68ba0d02ce46a22db7a4661440f2edf8037e2a23
karvaneg <m.lecarvennec@cbnsa.fr>
2025-10-24 07:44:27 +0200

    ♻️ F08: AddForm > extract image preview

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -8,6 +8,7 @@ import { useContext, useState } from "react";
 import OrderContext from "../../../../../../context/OrderContext";
 import TextInput from "../../../../../reusable-ui/TextInput";
 import Button from "../../../../../reusable-ui/Button";
+import ImagePreview from "./ImagePreview";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const EMPTY_PRODUCT = {
@@ -66,13 +67,10 @@ export default function AddForm() {
 
   return (
     <AddFormStyled onSubmit={handleSubmit}>
-      <div className="image-preview">
-        {newProduct.imageSource ? (
-          <img src={newProduct.imageSource} alt={newProduct.title} />
-        ) : (
-          <div className="empty-image">Aucune image</div>
-        )}
-      </div>
+      <ImagePreview
+        imageSource={newProduct.imageSource}
+        title={newProduct.title}
+      />
       <div className="input-fields">
         <TextInput
           name="title"
@@ -129,29 +127,6 @@ const AddFormStyled = styled.form`
   height: 100%;
   grid-column-gap: 20px;
   grid-row-gap: 8px;
-
-  .image-preview {
-    grid-area: 1 / 1 / 4 / 2;
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-
-    .empty-image {
-      height: 100%;
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border: 1px solid ${theme.colors.greyLight};
-      line-height: 1.5;
-      color: ${theme.colors.greySemiDark};
-      border-radius: ${theme.borderRadius.round};
-    }
-  }
 
   .input-fields {
     grid-area: 1 / 2 / -2 / -1;
