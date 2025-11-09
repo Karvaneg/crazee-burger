c18d558735313b0e144268e51aff736b0e324052
karvaneg <m.lecarvennec@cbnsa.fr>
2025-11-09 22:21:38 +0100

    💄 F10: BASKET > Add layout

---
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { theme } from "../../../../../theme";
+
+export default function Basket() {
+  return (
+    <BasketStyled>
+      <header>
+        <span>TOTAL</span>
+        <span>0,00 €</span>
+      </header>
+      <div className="basket-items">
+        {/* Basket items will be rendered here */}
+        Votre commande est vide.
+      </div>
+      <footer>Codé avec ❤️ et React.JS</footer>
+    </BasketStyled>
+  );
+}
+
+const BasketStyled = styled.div`
+  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  box-shadow: 0px 0px 20px 0px #00000033 inset;
+
+  background: ${theme.colors.background_white};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-family: ${theme.fonts.families.stylish};
+
+  header {
+    background: ${theme.colors.background_dark};
+    font-weight: ${theme.fonts.weights.regular};
+    font-size: ${theme.fonts.size.P4};
+    color: ${theme.colors.primary};
+    padding-top: 12px;
+    padding-right: 16px;
+    padding-left: 16px;
+    padding-bottom: 13px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .basket-items {
+    // overflow-y: scroll;
+    color: ${theme.colors.greyBlue};
+    font-weight: ${theme.fonts.weights.regular};
+    font-size: ${theme.fonts.size.P4};
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  footer {
+    background: ${theme.colors.background_dark};
+    color: ${theme.colors.text_white};
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    padding-top: 22.5px;
+    padding-bottom: 22.5px;
+    text-align: center;
+    vertical-align: middle;
+    font-weight: ${theme.fonts.weights.bold};
+    font-size: ${theme.fonts.size.P2};
+  }
+`;
